a55d4835848012721c76d2b69dba6dd2c13ec185
Flavien BERNARD <flavien.bernard09@gmail.com>
2020-12-21 11:52:52 +0400

    Add site packages for simulation with pygame

--- MachineLearning/bin/activate.fish
+++ MachineLearning/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/bertel/Documents/Robotique&IA/MachineLearning"
+set -gx VIRTUAL_ENV "/home/bertel/Documents/RobotiqueIA/MachineLearning"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
